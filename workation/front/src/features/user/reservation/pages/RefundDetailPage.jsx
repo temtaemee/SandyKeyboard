03a9f674cb296c0547e63dccb3b1e26aa2f7367f
DESKTOP-3CV852R\user1 <bo>
2026-05-08 15:11:57 +0900

    page파일 생성

---
+++ workation/front/src/features/user/reservation/pages/RefundDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RefundDetailPage() {
+  return (
+    <>
+      <h1>RefundDetailPage</h1>
+    </>
+  );
+}
+
+export default RefundDetailPage;
--- workation/front/src/routes/ResvRouter.jsx
+++ workation/front/src/routes/ResvRouter.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ReservationInsertPage from '../features/user/reservation/pages/ReservationInsertPage';
+import ReservationPaymentInsertPage from '../features/user/reservation/pages/ReservationPaymentInsertPage';
+import RefundDetailPage from '../features/user/reservation/pages/RefundDetailPage';
 
 function ResvRouter() {
   return (
     <>
-      <h1>ResvRouter</h1>
+      <Routes>
+        {/* reservation */}
+        <Route path="insert" element={<ReservationInsertPage />} />
+        <Route
+          path="payment/insert"
+          element={<ReservationPaymentInsertPage />}
+        />
+        <Route path="refund/detail/:id" element={<RefundDetailPage />} />
+      </Routes>
     </>
   );
 }
