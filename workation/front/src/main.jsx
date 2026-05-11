80afd52a69b10805117dce65f10a283309680719
DESKTOP-DB3FB27\user1 <wook's>
2026-05-11 14:09:33 +0900

    refactor : APP.jsx 파일 불필요한 import 삭제

--- workation/front/src/main.jsx
+++ workation/front/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 );
