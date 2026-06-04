be58c76b6b3e82ea62e83726f1380768e04b4687
DESKTOP-HUE315F\user1 <hello>
2026-06-04 16:28:41 +0900

    feat :  참여후기 프론트 변경 눌렀을때 색변경 후기목록,후기작성 / 희우

--- workation/front/src/features/user/board/review/pages/ReviewHomePage.jsx
+++ workation/front/src/features/user/board/review/pages/ReviewHomePage.jsx
@@ -1,18 +1,14 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { Wrapper, Title, TabContainer, Tab, WriteButton } from '../styles/ReviewHomePage.styles';
+import { Outlet } from 'react-router-dom';
+import { Wrapper, Title, TabContainer, Tab } from '../styles/ReviewHomePage.styles';
 
 export default function ReviewHomePage() {
-  const navigate = useNavigate();
-
   return (
     <Wrapper>
       <Title>참여후기</Title>
 
       <TabContainer>
-        <Tab to="/board/review">후기 목록</Tab>
-        <WriteButton onClick={() => navigate('/board/review/write')}>
-          후기 작성
-        </WriteButton>
+        <Tab to="/board/review" end>후기 목록</Tab>
+        <Tab to="/board/review/write">후기 작성</Tab>
       </TabContainer>
 
       <Outlet />
